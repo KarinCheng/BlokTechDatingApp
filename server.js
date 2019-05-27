c094d81c0e731218bb69c77750a46ed7e31dd1b0
KarinCheng <karin-cheng@hotmail.com>
2019-05-27 17:06:35 +0200

    basic routes

--- server.js
+++ server.js
@@ -1,10 +1,41 @@
-var d = new Date();
-d.getHours();
-d.getMinutes();
-d.getSeconds();
+/* eslint-disable semi */
 
-console.log(d);
+var fs = require("fs")
+var path = require("path")
+var http = require("http")
+var express = require("express");
+var app = express();
 
-const _ = require("lodash");
+app.set("view engine", "ejs");
 
-const _ = require("nodemon");
+var mime = {
+  ".ejs": "text/ejs",
+  ".css": "text/css"
+}
+
+http.createServer(onrequest).listen(8002)
+
+function onrequest(req, res) {
+  var route = req.url
+
+  if (route === "/") {
+    route = "index.ejs"
+  }
+
+  fs.readFile(path.join("static", route), onread)
+
+  function onread(err, buf) {
+    res.setHeader("Content-Type", "text/html")
+
+    if (err) {
+      res.statusCode = 404
+      res.end("Not found\n")
+    } else {
+      var extension = path.extname(route)
+      var type = mime[extension] || "text/plain"
+      res.statusCode = 200
+      res.setHeader("Content-Type", type)
+      res.end(buf)
+    }
+  }
+}

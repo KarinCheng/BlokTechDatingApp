8638a886906bc740446526160d26f5af393fb7ec
KarinCheng <karin-cheng@hotmail.com>
2019-05-30 22:35:18 +0200

    use ejs template

--- server.js
+++ server.js
@@ -1,41 +1,36 @@
 /* eslint-disable semi */
 
-var fs = require("fs")
-var path = require("path")
-var http = require("http")
-var express = require("express");
-var app = express();
+const express = require("express")
+const server = express()
+const port = 3000
 
-app.set("view engine", "ejs");
+server.set("view engine", "ejs")
 
 var mime = {
-  ".ejs": "text/ejs",
-  ".css": "text/css"
+  '.html': 'text/html',
+  '.css': 'text/css'
 }
 
-http.createServer(onrequest).listen(8002)
+server.get("/", (req, res) => {
+  res.render("index")
+})
 
-function onrequest(req, res) {
-  var route = req.url
+server.get("/filter", (req, res) => {
+  res.render("filter.ejs")
+})
 
-  if (route === "/") {
-    route = "index.ejs"
-  }
+server.get("/login", (req, res) => {
+  res.render("login.ejs")
+})
 
-  fs.readFile(path.join("static", route), onread)
+express()
+.use(notFound)
 
-  function onread(err, buf) {
-    res.setHeader("Content-Type", "text/html")
-
-    if (err) {
-      res.statusCode = 404
-      res.end("Not found\n")
-    } else {
-      var extension = path.extname(route)
-      var type = mime[extension] || "text/plain"
-      res.statusCode = 200
-      res.setHeader("Content-Type", type)
-      res.end(buf)
-    }
-  }
+function notFound (req, res) {
+  res.statusCode = 404
+  res.end("Not found\n")
 }
+
+server.use(express.static("public"))
+
+server.listen(port, () => console.log(`Example app listening on port ${port}!`))
